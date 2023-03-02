0d332983590083ea3d191de5d354ee2b987a4e5b
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2023-03-02 15:36:55 +0100

    meta endpoint

--- src/routes/kit-docs/[slug].meta/+server.ts
+++ src/routes/kit-docs/[slug].meta/+server.ts
@@ -1,3 +1,8 @@
 import { createMetaRequestHandler } from '@svelteness/kit-docs/node';
 
-export const GET = createMetaRequestHandler();
+export const GET = createMetaRequestHandler({
+  // map slug to absolute or relative file path to `routes` directory.
+  // returning `null` or `undefined` will fallback to default resolver.
+  // `resolve` helper will return default value.
+  resolve: (slug, { resolve }) => resolve(slug)
+});

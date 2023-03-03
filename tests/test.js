fb1982e2cf84cfd9a946eddc7a6bc5e45acaf349
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2023-03-03 04:24:45 +0100

    Fix tests error

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,10 @@
 import { expect, test } from '@playwright/test';
 
+const targetUrl = process.env.ENVIRONMENT_URL || 'https://efc-docs.vercel.app'
+
 test('visit page and take screenshot', async ({ page }) => {
   // If available, we set the target URL to a preview deployment URL provided by the ENVIRONMENT_URL created by Vercel.
   // Otherwise, we use the Production URL.
-  const targetUrl = process.env.ENVIRONMENT_URL || 'https://efc-docs.vercel.app'
 
   // We visit the page. This waits for the "load" event by default.
   const response = await page.goto(targetUrl)
@@ -17,6 +18,6 @@ test('visit page and take screenshot', async ({ page }) => {
 
 
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
+	await page.goto(targetUrl);
 	await expect(page.getByRole('heading', { name: 'About this documentation' })).toBeVisible();
 });
